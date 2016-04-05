d75bfa84c78c39970103db00da2788dd3b3fb710
mtuan <tranminhtuan684@gmail.com>
2016-04-05 23:18:57 +0700

    Initialize Performance Project

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,26 @@
+class EmployeesController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    
+  end
+end
